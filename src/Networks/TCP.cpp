c9232f90cabfdbc6a9632eedb77f877870f7851f
PaulaMaged <128493887+PaulaMaged@users.noreply.github.com>
2024-08-09 18:45:26 +0300

    change build directories inside .pri

---
+++ src/Networks/TCP.cpp
@@ -0,0 +1,6 @@
+#include "TCP.h"
+#include <iostream>
+
+void readData() {
+    qDebug() << "I am reading data";
+}
---
+++ src/Networks/TCP.h
@@ -0,0 +1,8 @@
+#ifndef TCP_H
+#define TCP_H
+
+#include <QUdpSocket>
+
+void readData();
+
+#endif
